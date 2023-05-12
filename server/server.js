e8d25b1f2b29c7d02f9c84a36aead6c249b4a57a
Yingson Yu <yingsonyu@gmail.com>
2023-05-12 13:46:17 -0500

    initial commit with base code

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const morgan = require("morgan");
+const parser = require("body-parser");
+
+app.use(express.static(path.join(__dirname, "..", "dist")));
+app.use("/assets", express.static(path.join(__dirname, "..", "assets")));
+app.use(morgan("dev"));
+
+app.use(express.json({ extended: true, limit: "30mb" }));
+app.use(express.urlencoded());
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+app.get("/*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "..", "dist/index.html"));
+});
+
+const port = process.env.PORT || 3004;
+
+app.listen(port, () => console.log("listening on port " + port));
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+import "./index.scss";
+
+const App = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
