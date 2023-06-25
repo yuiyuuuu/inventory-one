e4c74bef5e7f17411d2c6071593631358447e1b1
Yingson Yu <yingsonyu@gmail.com>
2023-06-24 19:54:56 -0500

    login page mock

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,41 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import "./index.scss";
+import "atropos/css";
 
 import Home from "./components/home/Home";
 import Stores from "./components/stores/Stores";
 import SingleStore from "./components/stores/SingleStore";
 import Sidebar from "./components/sidebar/Sidebar";
 import BurgerIcon from "./components/sidebar/BurgerIcon";
+import Login from "./components/auth/Login";
+
 import { dispatchSetSidebarState } from "./store/sidebar";
 
 const App = () => {
   const dispatch = useDispatch();
+
   const sidebarState = useSelector((state) => state.sidebarState);
 
   return (
     <div>
-      <BurgerIcon />
-      <Sidebar />
+      {!window.location.href.includes("login" || "signup") && <BurgerIcon />}
+
+      {!window.location.href.includes("login" || "signup") && <Sidebar />}
+
       <BrowserRouter>
         <div
-          className="side-blur"
+          className='side-blur'
           style={{
             zIndex: !sidebarState.display && -1000,
           }}
           onClick={() => dispatch(dispatchSetSidebarState({ display: false }))}
         />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/stores" element={<Stores />} />
-          <Route exact path="/stores/:id" element={<SingleStore />} />
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/stores' element={<Stores />} />
+          <Route exact path='/stores/:id' element={<SingleStore />} />
+          <Route exact path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+
+import $ from "jquery";
+
+import Atropos from "atropos";
+
+import "./auth.scss";
+
+const Login = () => {
+  useEffect(() => {
+    $(document).ready(() => {
+      // Initialize
+      const myAtropos = Atropos({
+        el: ".my-atropos",
+        shadow: false,
+      });
+    });
+  }, []);
+
+  return (
+    <div className='auth-parent'>
+      <div className='auth-inner'>
+        <div className='auth-sqcontainer'>
+          <div className='square'></div>
+          <div className='square'></div>
+          <div className='square'></div>
+          <div className='square'></div>
+          <div className='square'></div>
+          <div className='square'></div>
+        </div>
+        <div class='atropos my-atropos'>
+          <div class='atropos-scale'>
+            <div class='atropos-rotate'>
+              <div class='atropos-inner'>
+                <div className='auth-box'>
+                  <div className='container'>
+                    <div className='form'>
+                      <div className='auth-title'>Welcome to Invetory One</div>
+                      <form action='' className='auth-form'>
+                        <div className='auth-inputBx'>
+                          <input type='text' required='required' />
+                          <span>Login</span>
+                          <i className='fas fa-user-circle'></i>
+                        </div>
+                        <div className='auth-inputBx password'>
+                          <input
+                            id='password-input'
+                            type='password'
+                            name='password'
+                            required='required'
+                          />
+                          <span>Password</span>
+
+                          <i className='fas fa-key'></i>
+                        </div>
+                        <div className='auth-inputBx'>
+                          <input type='submit' value='Log in' disabled />
+                        </div>
+                      </form>
+
+                      <div>
+                        Don't have an account{" "}
+                        <a href='/signup' className='auth-redir'>
+                          Sign up
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
