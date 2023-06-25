932162950c42bbcc6954bd915dd11a89b04d63fb
Yingson Yu <yingsonyu@gmail.com>
2023-06-24 20:07:55 -0500

    icon fix

---
+++ src/components/customdateselector/CustomDateSelector.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./cds.scss";
+
+const CustomDateSelector = ({ selectedDate, func, top, left }) => {
+  return <div>CustomDateSelector</div>;
+};
+
+export default CustomDateSelector;
