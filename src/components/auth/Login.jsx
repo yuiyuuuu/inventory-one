3537df4bd79df458030e6194bc5b83b0a2f59083
yuiyuuuu <yingsonyu@gmail.com>
2023-06-26 16:15:25 -0500

    auth pages refactor

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -27,68 +27,73 @@ const Login = () => {
   }, [screenWidth]);
 
   return (
-    <div className='auth-parent'>
-      <div className='auth-inner'>
-        <div className='auth-sqcontainer'>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square2'></div>
-          <div className='square2'></div>
-          <div className='square2'></div>
+    <div className="auth-parent">
+      <div className="auth-inner">
+        <div className="auth-sqcontainer">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square2"></div>
+          <div className="square2"></div>
+          <div className="square2"></div>
         </div>
 
-        <div class='atropos my-atropos'>
-          <div class='atropos-scale'>
-            <div class='atropos-rotate'>
-              <div class='atropos-inner'>
-                <div className='auth-box'>
-                  <div className='container'>
-                    <div className='form'>
-                      <div className='auth-title'>Welcome to Inventory One</div>
-                      <div className='auth-subt'>
-                        By{" "}
-                        <a
-                          href='https://chicagocitysports.com/'
-                          target='_blank'
-                          rel='noopener noreferrer'
-                          className='auth-subt auth-a'
-                        >
-                          City Sports Chicago
-                        </a>
+        <div class="atropos my-atropos">
+          <div class="atropos-scale">
+            <div class="atropos-rotate">
+              <div class="atropos-inner">
+                <div className="auth-box">
+                  <div className="container">
+                    <div className="form">
+                      <a
+                        href="https://chicagocitysports.com/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        style={{ alignSelf: "center" }}
+                      >
+                        <img
+                          src="/assets/loginlogo.jpeg"
+                          className="auth-logo"
+                        />
+                      </a>
+                      <div className="auth-title">
+                        Welcome to{" "}
+                        <span style={{ fontWeight: "bold" }}>
+                          Inventory One
+                        </span>
                       </div>
-                      <form action='' className='auth-form'>
-                        <div className='auth-inputBx'>
+                      <form action="" className="auth-form">
+                        <div className="auth-inputBx">
                           <input
-                            type='text'
-                            required='required'
+                            type="text"
+                            required="required"
                             value={emailInput}
                             onChange={(e) => setEmailInput(e.target.value)}
                           />
                           <span>Email</span>
-                          <i className='fas fa-user-circle'></i>
+                          <i className="fas fa-user-circle"></i>
                         </div>
-                        <div className='auth-inputBx password'>
+                        <div className="auth-inputBx password">
                           <input
-                            id='password-input'
-                            type='password'
-                            name='password'
-                            required='required'
+                            id="password-input"
+                            type="password"
+                            name="password"
+                            required="required"
                             value={passInput}
                             onChange={(e) => setPassInput(e.target.value)}
                           />
                           <span>Password</span>
 
-                          <i className='fas fa-key'></i>
+                          <i className="fas fa-key"></i>
                         </div>
-                        <div className='auth-inputBx'>
-                          <input type='submit' value='Log in' disabled />
+                        <div className="auth-inputBx">
+                          <input type="submit" value="Log in" disabled />
                         </div>
                       </form>
 
-                      <div className='auth-dha'>
+                      <div className="auth-dha">
                         Don't have an account?{" "}
-                        <a href='/signup' className='auth-redir'>
+                        <a href="/signup" className="auth-redir">
                           Sign up
                         </a>
                       </div>
--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -27,57 +27,68 @@ const Signup = () => {
   }, [screenWidth]);
 
   return (
-    <div className='auth-parent'>
-      <div className='auth-inner'>
-        <div className='auth-sqcontainer'>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square'></div>
-          <div className='square2'></div>
-          <div className='square2'></div>
-          <div className='square2'></div>
+    <div className="auth-parent">
+      <div className="auth-inner">
+        <div className="auth-sqcontainer">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square2"></div>
+          <div className="square2"></div>
+          <div className="square2"></div>
         </div>
 
-        <div class='atropos my-atropos'>
-          <div class='atropos-scale'>
-            <div class='atropos-rotate'>
-              <div class='atropos-inner'>
-                <div className='auth-box'>
-                  <div className='container'>
-                    <div className='form'>
-                      <div className='auth-title'>Sign Up</div>
-                      <form action='' className='auth-form'>
-                        <div className='auth-inputBx'>
+        <div class="atropos my-atropos">
+          <div class="atropos-scale">
+            <div class="atropos-rotate">
+              <div class="atropos-inner">
+                <div className="auth-box">
+                  <div className="container">
+                    <div className="form">
+                      <a
+                        href="https://chicagocitysports.com/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        style={{ alignSelf: "center" }}
+                      >
+                        <img
+                          src="/assets/loginlogo.jpeg"
+                          className="auth-logo"
+                        />
+                      </a>
+                      <div className="auth-title">Sign Up</div>
+                      <form action="" className="auth-form">
+                        <div className="auth-inputBx">
                           <input
-                            type='text'
-                            required='required'
+                            type="text"
+                            required="required"
                             onChange={(e) => setEmailInput(e.target.value)}
                             value={emailInput}
                           />
                           <span>Email</span>
-                          <i className='fas fa-user-circle'></i>
+                          <i className="fas fa-user-circle"></i>
                         </div>
-                        <div className='auth-inputBx password'>
+                        <div className="auth-inputBx password">
                           <input
-                            id='password-input'
-                            type='password'
-                            name='password'
-                            required='required'
+                            id="password-input"
+                            type="password"
+                            name="password"
+                            required="required"
                             value={passInput}
                             onChange={(e) => setPassInput(e.target.value)}
                           />
                           <span>Password</span>
 
-                          <i className='fas fa-key'></i>
+                          <i className="fas fa-key"></i>
                         </div>
-                        <div className='auth-inputBx'>
-                          <input type='submit' value='Log in' disabled />
+                        <div className="auth-inputBx">
+                          <input type="submit" value="Sign Up" disabled />
                         </div>
                       </form>
 
-                      <div className='auth-dha'>
-                        Already have an account{" "}
-                        <a href='/login' className='auth-redir'>
+                      <div className="auth-dha">
+                        Already have an account?{" "}
+                        <a href="/login" className="auth-redir">
                           Login
                         </a>
                       </div>
