682888488256608529f6d928dffb504c02caccf0
yuiyuuuu <yingsonyu@gmail.com>
2023-07-19 09:48:47 -0500

    small - fix infinite spinner on not found lists

--- src/components/singlelist/SingleList.jsx
+++ src/components/singlelist/SingleList.jsx
@@ -33,6 +33,7 @@ const SingleList = () => {
   const [allProducts, setAllProducts] = useState([]);
 
   const [currentList, setCurrentList] = useState({});
+  const [listNotFound, setListNotFound] = useState(false);
 
   const [inputValue, setInputValue] = useState("");
   const [queryResults, setQueryResults] = useState([]);
@@ -87,6 +88,9 @@ const SingleList = () => {
         if (res.id) {
           setCurrentList(res);
           setAllProducts(res.item.sort((a, b) => a.name.localeCompare(b.name)));
+        } else {
+          setListNotFound(true);
+          setLoading(false);
         }
       })
       .catch(() => {
@@ -244,7 +248,7 @@ const SingleList = () => {
 
   if (loading) {
     return (
-      <div className='lds-ring lds-co' id='spinner-form'>
+      <div className="lds-ring lds-co" id="spinner-form">
         <div></div>
         <div></div>
         <div></div>
@@ -258,48 +262,62 @@ const SingleList = () => {
     ((authState.loading === "false" || !authState?.loading) && !authState?.id)
   ) {
     return (
-      <div className='home-parent'>
+      <div className="home-parent">
         <img
-          className='home-logo'
-          src='/assets/logo.jpeg'
+          className="home-logo"
+          src="/assets/logo.jpeg"
           onClick={() => (window.location.href = "/")}
           style={{ cursor: "pointer" }}
         />
 
-        <div className='home-krink'>{currentList?.name}</div>
+        <div className="home-krink">{currentList?.name}</div>
 
         <div>You do not have access to this list</div>
       </div>
     );
   }
 
+  if (listNotFound) {
+    return (
+      <div className="home-parent">
+        <img
+          className="home-logo"
+          src="/assets/logo.jpeg"
+          onClick={() => (window.location.href = "/")}
+          style={{ cursor: "pointer" }}
+        />
+        <div className="home-krink">List not found</div>
+      </div>
+    );
+  }
+
   return (
-    <div className='home-parent'>
+    <div className="home-parent">
       <img
-        className='home-logo'
-        src='/assets/logo.jpeg'
+        className="home-logo"
+        src="/assets/logo.jpeg"
         onClick={() => (window.location.href = "/")}
         style={{ cursor: "pointer" }}
       />
-      <div className='home-krink'>{currentList?.name}</div>
-      <div className='home-q'>
-        <div className='home-k'>
-          <div className='home-inparent'>
+      <div className="home-krink">{currentList?.name}</div>
+      <div className="home-q">
+        <div className="home-k">
+          <div className="home-inparent">
             <SearchSvg />
             <input
-              className='home-searchq'
+              className="home-searchq"
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
-              placeholder='Search'
-              id='home-search'
+              placeholder="Search"
+              id="home-search"
             />
           </div>
         </div>
       </div>
 
-      <div className='home-q home-t'>
+      <div className="home-q home-t">
         <button
-          className='home-add'
+          className="home-add"
           onClick={() => {
             setShowCreateOverlay(true);
             document.querySelector("html").style.overflow = "hidden";
@@ -309,7 +327,7 @@ const SingleList = () => {
         </button>
 
         <button
-          className='home-add'
+          className="home-add"
           style={{ marginLeft: "20px" }}
           onClick={() => {
             setShowMassOverlay(true);
@@ -320,7 +338,7 @@ const SingleList = () => {
         </button>
 
         <button
-          className='home-add home-export'
+          className="home-add home-export"
           style={{ marginLeft: "20px" }}
           onClick={() => handleExportExcel()}
         >
@@ -329,7 +347,7 @@ const SingleList = () => {
 
         {authState?.id === currentList?.owner?.id && (
           <button
-            className='home-add'
+            className="home-add"
             style={{ marginLeft: "20px" }}
             onClick={() => setShowShareOverlay(true)}
           >
@@ -339,9 +357,9 @@ const SingleList = () => {
       </div>
 
       {searchActive && !queryResults?.length ? (
-        <div className='home-no'>No products found</div>
+        <div className="home-no">No products found</div>
       ) : queryResults.length ? (
-        <div className='home-itemmap'>
+        <div className="home-itemmap">
           {queryResults?.map((item) => (
             <Item
               item={item}
@@ -357,7 +375,7 @@ const SingleList = () => {
           ))}
         </div>
       ) : allProducts.length ? (
-        <div className='home-itemmap'>
+        <div className="home-itemmap">
           {allProducts?.map((item) => (
             <Item
               item={item}
@@ -373,7 +391,7 @@ const SingleList = () => {
           ))}
         </div>
       ) : (
-        !loading && <div className='home-n'>No Products</div>
+        !loading && <div className="home-n">No Products</div>
       )}
 
       {showCreateOverlay && (
