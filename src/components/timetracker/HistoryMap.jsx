53c3c79f6a77f3e370469c09768416f81d47481e
yuiyuuuu <yingsonyu@gmail.com>
2023-10-11 21:03:17 -0500

    fix? time zone

--- src/components/timetracker/HistoryMap.jsx
+++ src/components/timetracker/HistoryMap.jsx
@@ -3,17 +3,9 @@ import React from "react";
 const HistoryMap = ({ tr }) => {
   return (
     <tr>
-      <td>
-        {new Date(tr.timeIn).toLocaleString("en-US", {
-          timeZone: "America/Chicago",
-        })}
-      </td>
+      <td>{new Date(tr.timeIn).toLocaleString("en-US")}</td>
 
-      <td>
-        {new Date(tr.timeOut).toLocaleString("en-US", {
-          timeZone: "America/Chicago",
-        })}
-      </td>
+      <td>{new Date(tr.timeOut).toLocaleString("en-US")}</td>
       <td>
         {(Math.abs(new Date(tr.timeIn) - new Date(tr.timeOut)) / 36e5).toFixed(
           2
