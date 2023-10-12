7d6873e0c2e5c26f3a53b1cdf258e565120d3aa8
yuiyuuuu <yingsonyu@gmail.com>
2023-10-11 21:21:54 -0500

    fux

--- src/components/timetracker/HistoryMap.jsx
+++ src/components/timetracker/HistoryMap.jsx
@@ -2,11 +2,20 @@ import React from "react";
 
 const HistoryMap = ({ tr }) => {
   console.log(tr);
+
   return (
     <tr>
-      <td>{new Date(tr.timeIn).toLocaleString("en-US")}</td>
+      <td>
+        {new Date(tr.timeIn).toLocaleString("en-US", {
+          timeZone: "America/Chicago",
+        })}
+      </td>
 
-      <td>{new Date(tr.timeOut).toLocaleString("en-US")}</td>
+      <td>
+        {new Date(tr.timeOut).toLocaleString("en-US", {
+          timeZone: "America/Chicago",
+        })}
+      </td>
       <td>
         {(Math.abs(new Date(tr.timeIn) - new Date(tr.timeOut)) / 36e5).toFixed(
           2
